b67df4c1ab39beca9dd740a544c899ed0e9c78aa
Abbas Magar <ali.abbas.magar@accenture.com>
2020-02-20 09:47:52 +0530

    Updates

---
+++ precache-manifest.37bd7f6cfb2f971eb2d3b8d7a6a51ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173c3b22ae1569174427ee842ca3f5d2",
+    "url": "/ProductReactApp-Snipcart/index.html"
+  },
+  {
+    "revision": "f6c5b53beb13d967067e",
+    "url": "/ProductReactApp-Snipcart/static/css/main.4f26f496.chunk.css"
+  },
+  {
+    "revision": "cc410de5155d2b29b5d9",
+    "url": "/ProductReactApp-Snipcart/static/js/2.03b837ca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ProductReactApp-Snipcart/static/js/2.03b837ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6c5b53beb13d967067e",
+    "url": "/ProductReactApp-Snipcart/static/js/main.f361c4ec.chunk.js"
+  },
+  {
+    "revision": "37be36305c4643880476",
+    "url": "/ProductReactApp-Snipcart/static/js/runtime-main.65131446.js"
+  }
+]);
